d6e5c51c10168f01cffe6dac1643750bc63e773d
Tommy <thuang20018@yahoo.com>
2021-05-21 15:05:55 -0400

    firebase cred

---
+++ src/components/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCEin_ElpJhE_Ir6OdlyzF_IQOGtKEktM0",
+    authDomain: "alexithymia-41ca0.firebaseapp.com",
+    databaseURL: "https://alexithymia-41ca0-default-rtdb.firebaseio.com",
+    projectId: "alexithymia-41ca0",
+    storageBucket: "alexithymia-41ca0.appspot.com",
+    messagingSenderId: "783835003645",
+    appId: "1:783835003645:web:3a8946ae15ac0809e0dc94"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
