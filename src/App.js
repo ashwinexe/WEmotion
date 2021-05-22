92f7c358f2dbb2f52e915d5dcab4a9c858e4242b
Tommy <thuang20018@yahoo.com>
2021-05-22 18:39:09 -0400

    added login to app.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import EmotionListDisplay from "./components/EmotionsListDisplay";
 import "./App.css";
 import Home from "./components/Home"
 import New from "./components/New"
-// import Login from "./components/Login"
+import Login from "./components/Login"
 import Signup from "./components/Signup"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import SampleFirebaseComponent from "./components/SampleFirebaseComponent";
@@ -17,6 +17,7 @@ const App = () => {
       <Switch>
         <Route path="/New"><EmotionListDisplay /></Route>
         <Route path="/Signup"><Signup /></Route>
+        <Route path="/Login"><Login /></Route>
         {/* <Route path="/Login"><Login /></Route> */}
         <Route path="/"><div>ALEXITHYMIA</div></Route>
       </Switch>
