486ba215d4f12be7e90516a7a29e71a1d99a7d44
isabiiil <isabelabonitalla@gmail.com>
2021-05-21 20:56:05 -0400

    WIP navbar, add hover effect on cards

--- src/App.js
+++ src/App.js
@@ -3,20 +3,30 @@ import {
   Card,
   CardContent,
   Typography,
+  Zoom,
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import { Navbar } from './components';
+import './App.css';
 
 const useAppStyles = makeStyles({
-  root: {
+  cards: {
     display: 'flex',
     justifyContent: 'space-evenly',
     flexWrap: 'wrap',
+    fontFamily: 'Open Sans',
   },
   card: {
-    minWidth: 275,
+    minWidth: 120,
     margin: '12px',
+    backgroundColor: '#d5adfb',
+    color: '#050831',
+    textAlign: 'center',
+  },
+  text: {
+    fontFamily: 'Open Sans',
   }
-})
+});
 
 export default function App() {
   const classes = useAppStyles();
@@ -28,21 +38,36 @@ export default function App() {
     'disgust',
     'sad',
     'happy',
-  ]
+  ];
+
+  function hover(e) {
+    e.target.style.background = '#133072';
+    e.target.style.color = '#FFFFFF';
+  }
+
+  function unhover(e) {
+    e.target.style.background = '#d5adfb';
+    e.target.style.color = '#050831';
+  }
 
   return (
-    <div className={classes.root}>
-      {emotionList.map((emotion) => {
-        return (
-          <Card className={classes.card}>
-            <CardContent>
-            <Typography variant="h5" component="h2">
-              {emotion}
-            </Typography>
-            </CardContent>
-          </Card>
-        )
-      })}
-    </div>
+    <>
+    <Navbar />
+      <div className={classes.cards}>
+        {emotionList.map((emotion, delay) => {
+          return (
+            <Zoom in="true" style={{ transitionDelay: delay }} timeout={800}>
+              <Card className={classes.card} onMouseEnter={hover} onMouseLeave={unhover}>
+                <CardContent>
+                  <Typography variant="h5" component="h2">
+                    {emotion}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Zoom>
+          )
+        })}
+      </div>
+    </>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import {
+  Link,
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useNavStyles = makeStyles({
+  root: {
+    backgroundColor: '#d5e6f7',
+    fontFamily: 'Open Sans',
+    fontSize: '32px',
+    color: '#050831',
+    position: 'relative',
+  },
+  link: {
+    color: '#050831',
+    margin: '32px 16px',
+    padding: '4px',
+    textDecoration: 'none',
+  },
+  crisis: {
+    backgroundColor: '#f90052',
+    color: '#050831',
+    position: 'absolute',
+    right: '16px',
+    padding: '0 4px',
+  },
+})
+
+export default function Navbar() {
+  const classes = useNavStyles();
+  const navbarList = [
+    'Home', // this is where the Alexithymia info will go
+    'New', // this is where the user will input new emotions
+    'Time', // this is where the calendar will go
+    'Settings', // this is a duh
+  ]
+
+  return (
+    <div className={classes.root}>
+      {navbarList.map((item) => {
+        return (
+          <Link className={classes.link} href="/">{item}</Link>
+        )
+      })}
+      <Link className={classes.crisis} href="https://suicidepreventionlifeline.org/">CRISIS</Link>
+    </div>
+  );
+}
