92eb62d41ca1cd59fa3955a6ca46e2061442229b
Isabel Abonitalla <isabel.abonitalla@oden.io>
2021-05-22 18:06:19 -0400

    fixed repeating emotionList, firebase build

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import EmotionListDisplay from "./components/EmotionsListDisplay";
 import "./App.css";
 import Home from "./components/Home"
 import New from "./components/New"
-import Login from "./components/Login"
+// import Login from "./components/Login"
 import Signup from "./components/Signup"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import SampleFirebaseComponent from "./components/SampleFirebaseComponent";
@@ -15,12 +15,12 @@ const App = () => {
     <Router>
       <Navbar></Navbar> 
       <Switch>
-        {/* <Route path="/"><Home /></Route> */}
-        <Route path="/New"><New /></Route>
+        <Route path="/New"><EmotionListDisplay /></Route>
         <Route path="/Signup"><Signup /></Route>
-        <Route path="/Login"><Login /></Route>
+        {/* <Route path="/Login"><Login /></Route> */}
+        <Route path="/"><div>ALEXITHYMIA</div></Route>
       </Switch>
-      <EmotionListDisplay /> 
+      {/* <EmotionListDisplay />  */}
       {/* component to display list of emotion */}
     </Router>
       {/* <SampleFirebaseComponent></SampleFirebaseComponent> */}
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import Typography from '@material-ui/core/Typography';
+import {
+  Avatar,
+  Button,
+  CssBaseline,
+  TextField,
+  Link,
+  Grid,
+  Box,
+  Typography,
+  Container,
+} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
 import heart from './img/heart.png'
 
 const useStyles = makeStyles((theme) => ({
@@ -116,9 +118,9 @@ export default function Signup() {
           </Button>
           <Grid container justify="flex-end">
             <Grid item>
-              <Link href="/Login">
+              {/* <Link href="/Login">
                 Already have an account? Sign in
-              </Link>
+              </Link> */}
             </Grid>
           </Grid>
         </form>
