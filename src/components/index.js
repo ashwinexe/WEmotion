be8d87a20d638cb84d2ea4b3833543e8df60694e
Isabel Abonitalla <isabel.abonitalla@oden.io>
2021-05-22 21:44:44 -0400

    index.js for cleaner imports

---
+++ src/components/index.js
@@ -0,0 +1,19 @@
+import EmotionCard from "./EmotionCard";
+import EmotionListDisplay from "./EmotionListDisplay";
+import Home from "./Home";
+import Login from "./Login";
+import Navbar from "./Navbar";
+import New from "./New";
+import Popup from "./popup";
+import Signup from "./Signup";
+
+export {
+  EmotionCard,
+  EmotionListDisplay,
+  Home,
+  Login,
+  Navbar,
+  New,
+  Popup,
+  Signup,
+}
